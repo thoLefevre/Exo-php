552158ae0452cfae94210cb3d9efd15f42e9d7a2
thoLefevre <90181755+thoLefevre@users.noreply.github.com>
2021-10-19 17:30:51 +0200

    EXO-PHP

---
+++ exo2.php
@@ -0,0 +1,18 @@
+<html>
+ <head> <title>formulaire.html</title> </head>
+<body>
+<?php
+echo
+$nombreAleatoire=rand ( 0 , 100 );
+if ($nombreAleatoire%2 == 1);
+ {
+   $color='red';
+ }  
+ else ($nombreAleatoire%1==1);
+ {
+    $color='blue'
+ }
+
+?>
+</body>
+    </html>
