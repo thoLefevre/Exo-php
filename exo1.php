dabd59d4bd36de5dc873b0dc665d7acd853d6835
thoLefevre <90181755+thoLefevre@users.noreply.github.com>
2021-10-21 14:22:34 +0200

    exo-php

---
+++ exo1.php
@@ -0,0 +1,16 @@
+<html>
+ <head> <title>formulaire.html</title> </head>
+<body>
+<form action="form1.php" method="post">
+ Votre nom: <input type="text" name="nom">
+ <br>
+ <input type="submit .....>
+ </form>
+</body>
+</html> 
+
+
+
+
+
+
